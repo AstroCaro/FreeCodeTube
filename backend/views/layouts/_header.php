d8b9106e6d483d28696900e16d379f1cbb1366a9
AstroCaro <silvagarcesc@gmail.com>
2023-01-08 08:12:21 -0300

    Adding a sidebar to main layout

---
+++ backend/views/layouts/_header.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+use yii\bootstrap5\Html;
+
+?>
+
+<header>
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
+        ],
+    ]);
+    $menuItems = [
+        ['label' => 'Home', 'url' => ['/site/index']],
+    ];
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    } else {
+        $menuItems[] = [
+            'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+            'url' => ['/site/logout'],
+            'options' => [
+                'class' => 'ms-auto'
+            ],
+            'linkOptions' => [
+                'data-method' => 'post'
+            ]
+        ];
+    }
+    //    echo \yii\helpers\Url::to(['/site/logout']);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav mb-2 mb-md-0 container-fluid'],
+        'items' => $menuItems,
+    ]);
+    if (Yii::$app->user->isGuest) {
+        echo Html::tag('div', Html::a('Login', ['/site/login'], ['class' => ['btn btn-link login text-decoration-none']]), ['class' => ['d-flex']]);
+    } //else {
+    //        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
+    //            . Html::submitButton(
+    //                'Logout (' . Yii::$app->user->identity->username . ')',
+    //                ['class' => 'btn btn-link logout text-decoration-none']
+    //            )
+    //            . Html::endForm();
+    //}
+    NavBar::end();
+    ?>
+
+</header>
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 
 /** @var \yii\web\View $this */
+
 /** @var string $content */
 
 use backend\assets\AppAsset;
@@ -13,81 +14,44 @@ use yii\bootstrap5\NavBar;
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>" class="h-100">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
 
-<header>
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = [
-            'label' => 'Logout ('.Yii::$app->user->identity->username.')',
-            'url' => ['/site/logout'],
-            'options' => [
-                'class' => 'ms-auto'
-            ],
-            'linkOptions' => [
-                'data-method' => 'post'
-            ]
-        ];
-    }
-//    echo \yii\helpers\Url::to(['/site/logout']);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav mb-2 mb-md-0 container-fluid'],
-        'items' => $menuItems,
-    ]);
-    if (Yii::$app->user->isGuest) {
-        echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
-    } else {
-//        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-//            . Html::submitButton(
-//                'Logout (' . Yii::$app->user->identity->username . ')',
-//                ['class' => 'btn btn-link logout text-decoration-none']
-//            )
-//            . Html::endForm();
-    }
-    NavBar::end();
-    ?>
-</header>
 
-<main role="main" class="flex-shrink-0">
-    <div class="container">
-<!--        --><?php //= Breadcrumbs::widget([
-//            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-//        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+    <body class="d-flex flex-column h-100">
+    <?php $this->beginBody() ?>
+
+    <div class="wrap h-100 d-flex flex-column">
+        <?php echo $this->render('_header'); ?>
+
+        <main role="main" class="flex-shrink-0">
+<!--            --><?php //echo $this->render('_sidebar'); ?>
+
+            <div class="container">
+                <!--        --><?php //= Breadcrumbs::widget([
+                //            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                //        ]) ?>
+                <?= Alert::widget() ?>
+                <?= $content ?>
+            </div>
+        </main>
     </div>
-</main>
 
-<footer class="footer mt-auto py-3 text-muted">
-    <div class="container">
-        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-end"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+    <footer class="footer mt-auto py-3 text-muted">
+        <div class="container">
+            <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+            <p class="float-end"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
 
-<?php $this->endBody() ?>
-</body>
-</html>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
 <?php $this->endPage();
